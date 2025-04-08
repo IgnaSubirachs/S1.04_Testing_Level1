57922662262512e7e93dc515ad606d9baae59c35
Ignasi Subirachs <ignasubirachs@gmail.com>
2025-04-08 10:28:23 +0200

    First Version Ex.3

---
+++ Ex.3/src/GestorArray.java
@@ -0,0 +1,6 @@
+public class GestorArray {
+
+    public int getElement(int[] list, int index) {
+        return list[index];
+    }
+}
---
+++ Ex.3/src/GestorArrayTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GestorArrayTest {
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testGetOutOfBounds() {
+        GestorArray testGestor = new GestorArray();
+        int[] array = {1, 2, 3,4};
+        testGestor.getElement(array, 1);
+    }
+}
